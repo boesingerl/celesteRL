8262931183cd01dbca833cffa0a068c96fa6a361
Léopaul Boesinger <leopaul.boesinger@epfl.ch>
2023-06-06 17:34:10 +0200

    cleaning python agent code

--- RLCode/rllib.py
+++ RLCode/rllib.py
@@ -1,10 +1,4 @@
-from celeste_rl.level import *
-from celeste_rl.env import *
-# from celeste_rl.models import *
-from rtgym import DEFAULT_CONFIG_DICT
-# from stable_baselines3.common.monitor import Monitor
-# from stable_baselines3 import PPO
-from collections import OrderedDict
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,16 +15,23 @@ import io
 import json
 import base64
 
-from celeste_rl.env import CelesteImgGym
 import gymnasium as gym
-from ray.rllib.algorithms.ppo import PPO, PPOConfig
 
+import ray
 
-from ray.air.integrations.wandb import WandbLoggerCallback
+from celeste_rl.env import CelesteImgGym
+from celeste_rl.level import *
+from celeste_rl.env import *
+from rtgym import DEFAULT_CONFIG_DICT
+from collections import OrderedDict
 
 from gymnasium.envs.registration import EnvSpec
-import ray
 from gymnasium.wrappers import TimeLimit
+
+from ray.rllib.algorithms.ppo import PPO, PPOConfig
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.tune.registry import register_env
+
 
 class EnvCreator:
     
@@ -43,23 +44,12 @@ class EnvCreator:
         return env
     
     
-from ray.tune.registry import register_env
+
 
 creator = EnvCreator()
 register_env("Celeste", creator.get_env)
 
 config = PPOConfig()
-
-# config.replay_buffer_config.update(
-#      {
-#          "capacity": 300000,
-#          "type": "MultiAgentReplayBuffer",
-#          "prioritized_replay": True,
-#          "prioritized_replay_alpha": 0.6,
-#           # Beta parameter for sampling from prioritized replay buffer.
-#          "prioritized_replay_beta": 0.4
-#      }
-# )
 
 config.model.update(use_lstm=True,
                     lstm_cell_size=64,
@@ -70,14 +60,6 @@ config.num_cpus_per_worker = 4
 config.train_batch_size = 10_000
 config.framework = 'tf2'
 
-# config.exploration_config = {
-#         "type": "SoftQ",
-#         "temperature": 1.0,
-#     }
-
-# config.lr = 1e-4
-        
-#config.compress_observations = True
 (config
  .training(
            lr_schedule=[[0, 1e-4],[1000*1_000, 5e-4], [2000*1_000, 3e-4], [10_000*1_000, 1e-4]]
@@ -90,7 +72,6 @@ config.framework = 'tf2'
 
 print(config.exploration_config)
 
-#algo = R2D2(config=config)  
 analysis = ray.tune.run(PPO,
                         config=config,
                         stop={"training_iteration": 10_000},
--- RLCode/startCeleste.bash
+++ RLCode/startCeleste.bash
@@ -1,6 +1,7 @@
+export DOTNET_ROOT=/usr/lib/dotnet/
 for i in $(seq 1 8);
 do
-    sleep 0.5
+    sleep 1
     ../../../Celeste &
 done
 
